29ea16e6e9c114de4b8576e82e99ad43faf07f0c
ItaloSeneAdv <italoseneadv@gmail.com>
2025-04-03 11:14:57 -0300

    Entire site and blog code
    In folders

---
+++ blogjs/blogstorage.js
@@ -0,0 +1,8 @@
+function getPosts() {
+    return JSON.parse(localStorage.getItem('posts') || '[]');
+  }
+  
+  function savePosts(posts) {
+    localStorage.setItem('posts', JSON.stringify(posts));
+  }
+  
